e7669fe308bce4dad6028306786a94ab0f86ab67
Vladimir Ivashchenko <48750889+vladmix77@users.noreply.github.com>
2020-04-01 11:24:49 +0300

    Create .arrow.svg

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,10 @@
 $(function() {
   $(".header__slider").slick({
     infinite: true,
-    fade: true
+    fade: true,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="" />',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="" />'
   });
 });
