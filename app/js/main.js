abc42a740355af1a91e6da9e243be3a55d3f90a0
Vladimir Ivashchenko <48750889+vladmix77@users.noreply.github.com>
2020-04-01 18:54:10 +0300

    Create ..slider-dots

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,13 @@ $(function() {
     prevArrow:
       '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="" />',
     nextArrow:
-      '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="" />'
+      '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="" />',
+    asNavFor: ".slider-dots"
+  });
+
+  $(".slider-dots").slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: ".header__slider"
   });
 });
