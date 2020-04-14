04bace666901e9f08c1c79a37667850ce1e793e4
Vladimir Ivashchenko <48750889+vladmix77@users.noreply.github.com>
2020-04-14 18:49:13 +0300

    Create .surf-box
    .slide-slick

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
   $(".header__slider").slick({
     infinite: true,
     fade: true,
@@ -6,17 +6,22 @@ $(function() {
       '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="" />',
     nextArrow:
       '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="" />',
-    asNavFor: ".slider-dots"
+    asNavFor: ".slider-dots",
   });
 
   $(".slider-dots").slick({
     slidesToShow: 4,
     slidesToScroll: 4,
-    asNavFor: ".header__slider"
+    asNavFor: ".header__slider",
   });
 
   $(".surf-slider").slick({
     slidesToShow: 4,
-    slidesToScroll: 1
-  })
+    slidesToScroll: 1,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="" />',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="" />',
+    asNavFor: ".slider-dots",
+  });
 });
