652d3825e4f7825130cd205d7f977d80ec136c0f
Vladimir Ivashchenko <48750889+vladmix77@users.noreply.github.com>
2020-04-16 11:53:30 +0300

    Create .slider-map(..slider-dots-...)
    point

--- app/js/main.js
+++ app/js/main.js
@@ -22,6 +22,14 @@ $(function () {
       '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="" />',
     nextArrow:
       '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="" />',
-    asNavFor: ".slider-dots",
+    asNavFor: ".slider-map",
+  });
+
+  $(".slider-map").slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: ".surf-slider",
+    focusOnSelect: true,
   });
 });
