8949a1407b3089fad858938c786a191f29545358
Vladimir Ivashchenko <48750889+vladmix77@users.noreply.github.com>
2020-04-30 23:30:06 +0300

    Create (- and +)
    .holder-pricing__span,
    .summ...
    
    js/scss/html

--- app/js/main.js
+++ app/js/main.js
@@ -41,4 +41,55 @@ $(function () {
     nextArrow:
       '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="" />',
   });
+
+  // --- (- and +) ---
+
+  $(
+    '<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="images/plus.svg" alt="" /></div><div class="quantity-button quantity-down"><img src="images/minus.svg" alt="" /></div></div>'
+  ).insertAfter(".quantity input");
+  $(".quantity").each(function () {
+    var spinner = $(this),
+      input = spinner.find('input[type="number"]'),
+      btnUp = spinner.find(".quantity-up"),
+      btnDown = spinner.find(".quantity-down"),
+      min = input.attr("min"),
+      max = input.attr("max");
+
+    btnUp.click(function () {
+      var oldValue = parseFloat(input.val());
+      if (oldValue >= max) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue + 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+
+    btnDown.click(function () {
+      var oldValue = parseFloat(input.val());
+      if (oldValue <= min) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue - 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+  });
+
+  // ----------------- пересчёт summ
+
+  $(".quantity-button").on("click", function () {
+    let summ =
+      $(".nights").val() * $(".summ").data("nights") +
+      ($(".guests").val() - 1) * $(".summ").data("guests");
+    $(".summ").html("$" + summ);
+  });
+
+  let summ =
+    $(".nights").val() * $(".summ").data("nights") +
+    ($(".guests").val() - 1) * $(".summ").data("guests");
+
+  $(".summ").html("$" + summ);
 });
